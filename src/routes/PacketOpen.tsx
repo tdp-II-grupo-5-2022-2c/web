3f8d01da1b7a4df1408f1707b58809448a433ab3
Daniela Carrero <yriveros@amazon.com>
2022-09-18 21:03:27 -0300

    TF-74 botón para ir a mis figuritas luego de abrir paquete y mejora visual de login

--- src/routes/PacketOpen.tsx
+++ src/routes/PacketOpen.tsx
@@ -1,14 +1,20 @@
 // @ts-nocheck
 import React from "react";
-import {Row, Container, Col, Fade} from "reactstrap";
+import {Row, Container, Col, Fade, Button} from "reactstrap";
 import Sticker from "../components/Sticker";
 import {getArgentinaPlayersData} from "../data/playersData";
+import {useNavigate} from "react-router-dom";
 
 function PacketOpen() {
   const fadeInTimeout = 600;
+  const navigate = useNavigate();
 
   const getStickers = () => {
-    return getArgentinaPlayersData().slice(0,5)
+    return getArgentinaPlayersData().slice(0, 5)
+  }
+
+  const closePacket = () => {
+    navigate('/my-stickers');
   }
 
   return (
@@ -16,15 +22,21 @@ function PacketOpen() {
         <Container fluid>
           <Row className="m-5 h-100 justify-content-center">
             {getStickers().map((player, index) =>
-              <Col key={index} className={`m-3 col-10 col-sm-10 col-md-10 col-lg-3 col-xl-3 d-flex justify-content-center ${index % 2 ? "align-bottom" : "align-top"}`}>
-                <Fade appear={true} timeout={index * fadeInTimeout}>
-                  <Sticker
-                      player={player}
-                      readOnly={true}
-                  />
-                </Fade>
-              </Col>
+                <Col key={index}
+                     className={`m-3 col-10 col-sm-10 col-md-10 col-lg-3 col-xl-3 d-flex justify-content-center ${index % 2 ? "align-bottom" : "align-top"}`}>
+                  <Fade appear={true} timeout={index * fadeInTimeout}>
+                    <Sticker
+                        player={player}
+                        readOnly={true}
+                    />
+                  </Fade>
+                </Col>
             )}
+          </Row>
+          <Row className="justify-content-center">
+            <Col className="mb-3 col-6 col-sm-6 col-md-3 col-lg-3 col-xl-3 d-flex justify-content-center">
+              <Button size="lg" color="success" onClick={closePacket}>OK</Button>
+            </Col>
           </Row>
         </Container>
       </>
--- src/routes/SignIn.tsx
+++ src/routes/SignIn.tsx
@@ -3,6 +3,7 @@ import '../css/App.css';
 import MyNavbar from "../components/MyNavbar";
 import { useAuth } from "../context/authContext";
 import { useNavigate } from "react-router-dom";
+import {Button, Card, CardHeader, CardBody, Col, Row} from "reactstrap";
 
 export function SignIn() {
   const [user, setUser] = useState({
@@ -52,28 +53,77 @@ export function SignIn() {
   return (
     <React.Fragment>
       <MyNavbar/>
-      <div className="body text-center">
-        {error && <p>{error}</p>}
-        <main className="form-signin w-100 m-auto">
-          <form onSubmit={handleSubmit}>
-            <img className="mb-4" src={require("../res/img/qatar.png")} alt="" width="72" height="57"/>
-            <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
-            <div className="form-floating">
-              <input type="email" name='email' className="form-control" id="floatingInput" placeholder="name@example.com" onChange={handleChange}/>
-              <label htmlFor="floatingInput">Email address</label>
-            </div>
-            <div className="form-floating">
-              <input type="password" name='password' className="form-control" id="floatingPassword" placeholder="Password" onChange={handleChange}/>
-              <label htmlFor="floatingPassword">Password</label>
-            </div>
-            <button className="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
-            
-          </form>
-          <button onClick={handleGoogleSignIn}> Login with google </button>
-          <button onClick={handleRegister}> Register </button>
-          <p className="mt-5 mb-3 text-muted">&copy; 2017–2022</p>
-        </main>
-      </div>
+      <Row className="justify-content-center">
+        <Col lg="5" md="7">
+          <Card className="shadow border-0">
+            {error && <p>{error}</p>}
+            <CardHeader className="bg-transparent pb-5">
+              <Row>
+                <div className="text-muted text-center mt-2 mb-3">
+                  <small>Inicia sesión con</small>
+                </div>
+                <div className="btn-wrapper text-center">
+                  <Button
+                      className="btn-neutral btn-icon"
+                      color="default"
+                      onClick={handleGoogleSignIn}
+                  >
+                        <span className="btn-inner--icon">
+                          <img
+                              alt="..."
+                              src={
+                                require("../assets/img/icons/common/google.svg")
+                                    .default
+                              }
+                          />
+                        </span>
+                    <span className="btn-inner--text">Google</span>
+                  </Button>
+                </div>
+              </Row>
+            </CardHeader>
+            <CardBody className="px-lg-5 py-lg-5">
+              <div className="text-center text-muted mb-4">
+                <small>O inicia sesión con credenciales</small>
+              </div>
+
+              <div className="text-center">
+                <img className="mb-4" src={require("../res/img/qatar.png")} alt="" width="72" height="57"/>
+              </div>
+              <Row className="form-signin w-100 m-auto">
+                <form onSubmit={handleSubmit}>
+
+                  <h1 className="h3 mb-3 fw-normal text-center">Inicia Sesión</h1>
+                  <div className="form-floating">
+                    <input type="email" name='email' className="form-control" id="floatingInput" placeholder="nombre@ejemplo.com" onChange={handleChange}/>
+                    <label htmlFor="floatingInput">Email</label>
+                  </div>
+                  <div className="form-floating">
+                    <input type="password" name='password' className="form-control" id="floatingPassword" placeholder="Contraseña" onChange={handleChange}/>
+                    <label htmlFor="floatingPassword">Contraseña</label>
+                  </div>
+                  <Row>
+                    <Col className="text-center">
+                      <Button color="primary">Iniciar Sesión</Button>
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col className="text-center">
+                      <Button
+                          className="btn-neutral btn-icon"
+                          color="link"
+                          onClick={handleRegister}
+                      >
+                        <span className="btn-inner--text">Register</span>
+                      </Button>
+                    </Col>
+                  </Row>
+                </form>
+              </Row>
+            </CardBody>
+          </Card>
+        </Col>
+      </Row>
     </React.Fragment>
   )
 }
