a3013ffc9a214b523f00650c85d1672408370170
AgustinLeguizamon <agustinmleguizamon@gmail.com>
2022-10-23 20:02:44 -0300

    TF-102 se agregan api calls para aceptar y rechazar intercambio

--- src/components/CommunityExchanges.tsx
+++ src/components/CommunityExchanges.tsx
@@ -2,10 +2,20 @@ import React, {useEffect, useState} from "react";
 import Exchange, {IExchange} from "./Exchange";
 import {useUser} from "../context/UserContext";
 import {getMockedExchanges} from "../data/exchangesData";
+import {MyModal2} from "./MyModal";
+import {ExchangeStrings} from "../res/strings";
+import client from "../services/config";
 
 const CommunityExchanges = () => {
   const user = useUser()
   const [communityExchanges, setCommunityExchanges] = useState<IExchange[]>([])
+
+  const initialModalState = {
+    header: "",
+    body: "",
+  };
+  const [modal, setModal] = useState(initialModalState)
+  const [showModal, setShowModal] = useState(false)
 
   useEffect(() => {
     _fetchCommunityExchanges()
@@ -16,29 +26,74 @@ const CommunityExchanges = () => {
     setCommunityExchanges(getMockedExchanges())
   }
 
-  const acceptExchange = (exchangeId: string) => {
+  const acceptExchange = async (exchangeId: string) => {
     console.log("Exchange accepted! id " + exchangeId)
+    const success = await doRequest("accept")
+    if (success) {
+      setModal({header: "", body: ExchangeStrings.EXCHANGE_ACCEPT_OK})
+    } else {
+      setModal({header: "", body: ExchangeStrings.EXCHANGE_ACCEPT_ERROR})
+    }
+    setShowModal(true)
   }
 
-  const rejectExchange = (exchangeId: string) => {
+  const rejectExchange = async (exchangeId: string) => {
     console.log("Exchange rejected! id " + exchangeId)
+    const success = await doRequest("accept")
+    if (!success) {
+      setModal({header: "", body: ExchangeStrings.EXCHANGE_REJECT_ERROR})
+    }
+    setShowModal(true)
   }
 
-  return(
-    <div className="container">
-      <div className="row">
-        <div className="col">
+  const closeModal = () => {
+    // Fetcheo de nuevo para actualizar y que NO se vea mas el intercambio rechazado o aceptado
+    setShowModal(false)
+    _fetchCommunityExchanges()
+  }
+
+  const doRequest = async (action: string): Promise<boolean> => {
+    const form = {
+      action: action,
+      sender_id: user._id
+    }
+    try {
+      const {data: response} = await client.post("/exchanges", form)
+      return true
+
+    } catch (error: any) {
+      if (error.response) {
+        if (error.response.data?.detail === "TODO: detalle error back") {
+
+        }
+        console.log(error.response);
+      } else if (error.request) {
+        console.log(error.request);
+      } else {
+        console.log('Error', error.message);
+      }
+    }
+
+    return false
+  }
+
+
+  return (
+    <React.Fragment>
+      <div className="container">
+        <div className="row">
           {communityExchanges.map((exchange, index) =>
-            <div key={exchange._id}>
-              <Exchange exchange={exchange} isOwner={false} onAccept={acceptExchange} onReject={rejectExchange}/>
+            <div className="col d-flex justify-content-center">
+              <div key={exchange._id}>
+                <Exchange exchange={exchange} isOwner={false} onAccept={acceptExchange} onReject={rejectExchange}/>
+              </div>
             </div>
           )}
         </div>
       </div>
-
-    </div>
+      <MyModal2 modal={modal} isOpen={showModal} onAccept={closeModal}/>
+    </React.Fragment>
   )
-
 }
 
 export default CommunityExchanges
--- src/components/Exchange.tsx
+++ src/components/Exchange.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import {StickerStack, StickerStack2} from "./stickers/StickerStack";
 import PlayersInfo from "./stickers/PlayersInfo";
 import {Button} from "reactstrap";
-import {globalButtonsStyle} from "../res/globalStyles";
+import {debugStyle, globalButtonsStyle} from "../res/globalStyles";
 
 export type IExchange = {
   _id: string,
@@ -29,36 +29,42 @@ const Exchange = ({exchange, isOwner = true, onAccept, onReject}: Props) => {
       backgroundImage: `url("/images/bg_exchange.jpg")`
     },
     arrows: {
-      fontSize: "50px"
+      fontSize: "70px",
     }
   }
 
   return (
     <div className="card" style={styles.exchange}>
       <div className="card-body text-center">
-        <div className="row">
-          <h1 className="text-white">Jessica Jones</h1>
-        </div>
-        <div className="row">
-          <div className="col">
-            <StickerStack stickers={exchange.stickers_to_give} offset={0.5}/>
-            <PlayersInfo stickers={exchange.stickers_to_give}/>
+        <div className="row mb-1">
+          <div className="col-3">
             {!isOwner && onAccept &&
                 <Button style={globalButtonsStyle.alternative} onClick={() => onAccept(exchange._id)}>
                     <p className="text-white m-0">Aceptar</p>
                 </Button>}
           </div>
-          <div className="col position-absolute">
-            <h1 className="text-red" style={styles.arrows}>{"->"}</h1>
-            <h1 className="text-green" style={styles.arrows}>{"<-"}</h1>
+          <div className="col-6">
+            <h1 className="text-white">Jessica Jones</h1>
           </div>
+          <div className="col-3">
+            {!isOwner && onReject &&
+                <Button style={globalButtonsStyle.white} onClick={() => onReject(exchange._id)}>
+                    <p className="text-qatar-secondary m-0">Rechazar</p>
+                </Button>}
+          </div>
+        </div>
+        <div className="row">
           <div className="col">
+            <StickerStack stickers={exchange.stickers_to_give} offset={0.5}/>
+            <PlayersInfo stickers={exchange.stickers_to_give}/>
+          </div>
+          <div className="col position-absolute">
+            <h1 className="text-red" style={styles.arrows}>{">"}</h1>
+            <h1 className="text-green" style={styles.arrows}>{"<"}</h1>
+          </div>
+          <div className="col ml-2">
             <StickerStack2 stickers={exchange.stickers_to_receive}/>
             <PlayersInfo stickers={exchange.stickers_to_receive}/>
-            {!isOwner && onReject &&
-              <Button style={globalButtonsStyle.white} onClick={() => onReject(exchange._id)}>
-                <p className="text-qatar-secondary m-0">Rechazar</p>
-            </Button>}
           </div>
         </div>
       </div>
