95a30ce20198a9157f4438b4c5c3e5ecffe7983d
AgustinLeguizamon <agustinmleguizamon@gmail.com>
2022-11-20 17:06:34 -0300

    TF-196 se agrega spinner en Mis intercambios y en Mis Comunidades

--- src/routes/MyCommunities.tsx
+++ src/routes/MyCommunities.tsx
@@ -1,15 +1,7 @@
 import React, {useEffect, useState} from "react";
 import MyNavbar from "../components/MyNavbar";
 import {useUser} from "../context/UserContext";
-import {
-  Badge,
-  Button,
-  Col,
-  Container,
-  ListGroup,
-  ListGroupItem,
-  Row
-} from "reactstrap";
+import {Badge, Button, Col, Container, ListGroup, ListGroupItem, Row} from "reactstrap";
 import CreateCommunityModal, {CreateCommunityForm} from "../components/CreateCommunityModal";
 import client from "../services/config";
 import {CommunityCreationStrings} from "../res/strings";
@@ -17,6 +9,7 @@ import {useNavigate} from "react-router-dom";
 import {fetchCommunities} from "../services/apicalls";
 import Success from "../components/modals/Success";
 import Error from "../components/modals/Error";
+import MySpinner from "../components/spinner/MySpinner";
 
 export type ICommunity = {
   "_id": string,
@@ -46,11 +39,20 @@ const MyCommunities = () => {
   const [createdCommunityId, setCreatedCommunityId] = useState<string>();
   const [isDesktop, setDesktop] = useState<boolean>(window.innerWidth >= DESKTOP_SIZE);
 
+  const [isLoading, setIsLoading] = useState(false)
+
   const navigate = useNavigate();
 
   useEffect(() => {
-    fetchCommunities(undefined, user._id).then(response => setCommunities(response))
+    _fetchCommunities()
   }, [])
+
+  const _fetchCommunities = async () => {
+    setIsLoading(true)
+    const fetchedCommunities = await fetchCommunities(undefined, user._id)
+    setCommunities(fetchedCommunities)
+    setIsLoading(false)
+  }
 
   const isFormValid = () => {
     return (createCommunityForm.name.length > 0 && createCommunityForm.password.length > 0)
@@ -75,19 +77,24 @@ const MyCommunities = () => {
       setCreatedCommunityId(createdCommunity._id);
       setModalOk({header: CommunityCreationStrings.OK_HEADER, body: CommunityCreationStrings.CREATED})
       setShowModalOk(true)
-      fetchCommunities()
     } catch (error: any) {
       if (error.response) {
-        if (error.response.data?.detail.includes("there is already a community with that name")){
-          setModalError({header:CommunityCreationStrings.ERROR_HEAD, body: CommunityCreationStrings.COMMUNITY_ALREADY_EXISTS})
+        if (error.response.data?.detail.includes("there is already a community with that name")) {
+          setModalError({
+            header: CommunityCreationStrings.ERROR_HEAD,
+            body: CommunityCreationStrings.COMMUNITY_ALREADY_EXISTS
+          })
           setShowModalError(true)
         }
         if (error.response.data?.detail.includes("user can't be in more than 10 communities")) {
-          setModalError({header:CommunityCreationStrings.ERROR_HEAD, body: CommunityCreationStrings.COMMUNITY_LIMIT})
+          setModalError({header: CommunityCreationStrings.ERROR_HEAD, body: CommunityCreationStrings.COMMUNITY_LIMIT})
           setShowModalError(true)
         }
         if (error.response.data?.detail.includes("has not complete his profile")) {
-          setModalError({header:CommunityCreationStrings.ERROR_HEAD, body: CommunityCreationStrings.PROFILE_NOT_COMPLETED})
+          setModalError({
+            header: CommunityCreationStrings.ERROR_HEAD,
+            body: CommunityCreationStrings.PROFILE_NOT_COMPLETED
+          })
           setShowModalError(true)
         }
         console.log(error.response);
@@ -100,8 +107,11 @@ const MyCommunities = () => {
   }
 
   const onCreateCommunityClick = async () => {
-    if(!user.is_profile_complete){
-      setModalError({header:CommunityCreationStrings.PROFILE_NOT_COMPLETED_TITLE, body: CommunityCreationStrings.PROFILE_NOT_COMPLETED})
+    if (!user.is_profile_complete) {
+      setModalError({
+        header: CommunityCreationStrings.PROFILE_NOT_COMPLETED_TITLE,
+        body: CommunityCreationStrings.PROFILE_NOT_COMPLETED
+      })
       setShowModalError(true)
       return
     }
@@ -179,18 +189,24 @@ const MyCommunities = () => {
             {/*  </Form>*/}
             {/*</Row>*/}
             <ListGroup>
-            {fetchedCommunities
-                .sort((a,b) => a.owner === user._id ? -1 : 0)
-                .map((community, index) =>
-            <ListGroupItem className="justify-content-between" key={community._id} action onClick={viewCommunity} id={community._id} tag="button">
-              <span id={community._id} className="mr-2">{community.name}</span>
-              {user._id === community.owner &&
-                  <Badge id={community._id} pill color="default">
-                      ADMIN
-                  </Badge>
+              {isLoading &&
+                <div className="d-flex justify-content-center align-items-center h-65vh">
+                  <MySpinner className="text-white-50" message={"Obteniendo comunidades..."}/>
+                </div>
               }
-            </ListGroupItem>
-            )}
+              {!isLoading && fetchedCommunities
+                .sort((a, b) => a.owner === user._id ? -1 : 0)
+                .map((community, index) =>
+                  <ListGroupItem className="justify-content-between" key={community._id} action onClick={viewCommunity}
+                                 id={community._id} tag="button">
+                    <span id={community._id} className="mr-2">{community.name}</span>
+                    {user._id === community.owner &&
+                      <Badge id={community._id} pill color="default">
+                        ADMIN
+                      </Badge>
+                    }
+                  </ListGroupItem>
+                )}
             </ListGroup>
           </Col>
         </Row>
@@ -200,8 +216,8 @@ const MyCommunities = () => {
                             isOpen={showCreateCommunityFormModal}
                             onAccept={createCommunity}
                             onCancel={() => {
-                   setShowCreateCommunityFormModal(false)
-                 }}
+                              setShowCreateCommunityFormModal(false)
+                            }}
                             form={createCommunityForm}
                             handleChange={handleChange}
       />
